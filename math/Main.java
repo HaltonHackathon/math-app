1f57db4f99aa7a145d9374450e0ad89875dd984c
sharpstewie <aghaparsa@gmail.com>
2015-05-20 10:08:03 -0400

    main method

---
+++ math/Main.java
@@ -0,0 +1,11 @@
+package hackathon2015;
+
+import java.util.*;
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet {
+	public void paint (Graphics g){
+		g.drawRect(, y, width, height);
+	}
+}
