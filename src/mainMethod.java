469a917fedfdcf847d62be410066c20458e458c8
sharpstewie <aghaparsa@gmail.com>
2015-05-20 11:57:18 -0400

    helo

--- src/mainMethod.java
+++ src/mainMethod.java
@@ -1,29 +1,65 @@
 import java.awt.*;
-
+import java.awt.event.*;
 
 import javax.swing.*;
 
-public class mainMethod extends JApplet {
+public class mainMethod extends JApplet implements ComponentListener{
 	protected JPanel graph = new JPanel(), value = new JPanel(), buttons = new JPanel();
+	
+	Toolkit tk = Toolkit.getDefaultToolkit();
+	protected static Dimension d;
+	
+	protected void setDim(){
+		d = tk.getScreenSize();
+		System.out.println("Width = " + d.getWidth() + " + Height = " + d.getHeight());
+	}
+	
 	
 	public void init(){
 			Container main = getContentPane();
-			main.setLayout(null);;
+			main.add(graph);
+			main.add(value);
+			main.add(buttons);
+			main.setLayout(null);
 			
+//			graph.setBounds(0, 0, width, height);
 			
-		
+			main.addComponentListener(new ComponentListener(){
+				public void componentResized(ComponentEvent e){
+					setDim();
+				}
+
+				@Override
+				public void componentMoved(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void componentShown(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void componentHidden(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
 	}
 	public void paint (Graphics g){
 		
 	}
 	
+	
+	
 	public static void main (String[] args){
-		Toolkit tk = Toolkit.getDefaultToolkit();
-	    Dimension d = tk.getScreenSize();
+		
 	    System.out.println("Screen width = " + d.width);
 	    System.out.println("Screen height = " + d.height);
 	    
-	    run(new mainMethod(), d.height, d.height*4/3);
+	    run(new mainMethod(), (int) d.getWidth(), (int) d.height*4/3);
 	}
 	
 	public static void run(JApplet applet, int width, int height){
@@ -34,6 +70,34 @@ public class mainMethod extends JApplet {
 	    applet.init();
 	    applet.start();
 	    frame.setVisible(true);
+	}
+
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
